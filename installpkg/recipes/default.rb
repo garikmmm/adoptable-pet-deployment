dbb0888d11c1ad780ac85349d21f6ffa736652ab
Garegin Hambardzumyan <gareginhambardzumyan@MacBook-Pro.local>
2015-09-25 17:31:23 +0400

    Fixing apache restart after php module installation

--- installpkg/recipes/default.rb
+++ installpkg/recipes/default.rb
@@ -1,12 +1,11 @@
+include_recipe 'apache2::service'
+
 package 'php5-redis' do
   action :install
 end
 
 package 'php5-intl' do
   action :install
-end
-
-include_recipe 'apache2::service'
-service 'apache2' do
   notifies :restart, resources(:service => 'apache2')
 end
+

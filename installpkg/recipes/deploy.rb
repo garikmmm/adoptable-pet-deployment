5e4c3ea197803d34577e5d25500b1c26f68b551f
Garegin Hambardzumyan <garegin@greatergood.com>
2015-10-06 17:19:54 +0400

    removed composer.phar download part from deployment

--- installpkg/recipes/deploy.rb
+++ installpkg/recipes/deploy.rb
@@ -1,6 +1,5 @@
 #
-# Taken from:
-# http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.photoapp.3.html
+#    curl -s https://getcomposer.org/installer | php
 #
 node[:deploy].each do |application, deploy|
   script "install_composer" do
@@ -8,7 +7,6 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-    curl -s https://getcomposer.org/installer | php
     php composer.phar install --no-dev --no-interaction --optimize-autoloader
     EOH
     only_if { ::File.exists?("#{deploy[:deploy_to]}/current/composer.json") }

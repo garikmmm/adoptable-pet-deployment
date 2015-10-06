62328aadec1e3cd26b39589f560901d831c52e0d
Garegin Hambardzumyan <garegin@greatergood.com>
2015-10-06 17:58:57 +0400

    fixing deploy script

--- installpkg/recipes/deploy.rb
+++ installpkg/recipes/deploy.rb
@@ -1,5 +1,6 @@
 #
-#    curl -s https://getcomposer.org/installer | php
+# Taken from:
+# http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.photoapp.3.html
 #
 node[:deploy].each do |application, deploy|
   script "install_composer" do
@@ -7,6 +8,7 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
+    curl -s https://getcomposer.org/installer | php
     php composer.phar install --no-dev --no-interaction --optimize-autoloader
     EOH
     only_if { ::File.exists?("#{deploy[:deploy_to]}/current/composer.json") }
